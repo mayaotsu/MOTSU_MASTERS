e690e164fabe212ab5aec7fdc6b1dff8f3056542
mayaotsu <otsumaya@hawaii.edu>
2024-12-10 12:58:02 -1000

    create new density and distribution map

---
+++ script/density_distribution_map.R
@@ -0,0 +1,44 @@
+#density
+spc_reduced %>% 
+  filter(density>0) %>% 
+  mutate(lon=round(lon, 1),
+       (lat=round(lat, 1))) %>% 
+  group_by(lon, lat, island, species) %>% 
+  summarise(density=mean(density)) %>% 
+  ggplot(aes(lon, lat, size = density, fill = species)) + 
+  geom_point(shape = 21) #+
+ # facet_wrap(~island, scale = "free")
+
+#presence
+spc_reduced %>% 
+  filter(density>0) %>% 
+  mutate(lon=round(lon, 2),
+         (lat=round(lat, 2))) %>% 
+  group_by(lon, lat, island, species) %>% 
+  summarise(density=mean(density)) %>% 
+  ggplot(aes(lon, lat, fill = species)) + 
+  geom_point(shape = 21) +
+  facet_wrap(~island, scale = "free")
+
+#depth profile
+library(patchwork)
+p1 = spc_reduced %>% 
+  filter(species=="LUFU") %>% 
+  ggplot(aes(depth, density, colour = species)) + 
+  geom_smooth(method = "loess", span=1, show.legend = FALSE) +
+  ggtitle("Toʻau") +
+  labs(y = "density per 100 m^2", x = "depth (m)") +
+  theme_classic(base_size = 15)
+
+p2 = spc_reduced %>% 
+  filter(species=="LUKA") %>% 
+  ggplot(aes(depth, density)) + 
+  geom_smooth(method = "loess", span=1, show.legend = FALSE) +
+  ggtitle("Taʻape") +
+  labs(y = "density per 100 m^2", x = "depth (m)") +
+  theme_classic(base_size = 15)
+
+p1+p2
+sggsave(last_plot(), file = ""
+
+

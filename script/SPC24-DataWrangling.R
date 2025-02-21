bacbb05c1d03dcf281c7c1318c386f8860e5408a
mayaotsu <otsumaya@hawaii.edu>
2025-02-20 15:54:19 -1000

    update for 2024 ncrmp data wrangling and renaming spc files

--- script/SPC24-DataWrangling.R
+++ script/SPC24-DataWrangling.R
@@ -1,27 +1,44 @@
 ## DATA CLEANING FOR NEW 2024 NCRMMP
 rm(list = ls())
+library(dplyr)
+library(lubridate)
 load("/Users/mayaotsu/Downloads/ALL_REA_FISH_RAW.rdata")
 
 colnames(df)
-sum(df$SPECIES == "LUKA", na.rm = TRUE) #4216
-sum(df$SPECIES == "LUFU", na.rm = TRUE) #1956
-sum(df$SPECIES == "CEAR", na.rm = TRUE) #12056
+sum(df$SPECIES == "LUKA", na.rm = TRUE) #4216, 2536
+sum(df$SPECIES == "LUFU", na.rm = TRUE) #1956, 974
+sum(df$SPECIES == "CEAR", na.rm = TRUE) #12056, 2340
 
 species <- c("Lutjanus kasmira", "Cephalopholis argus", "Lutjanus fulvus")
 islands <- c("Hawaii", "Kahoolawe", "Kauai", "Lanai", "Maui", "Molokai", "Niihau", "Oahu",
              "French Frigate", "Gardner", "Kure", "Laysan", "Lisianski", "Maro", "Midway", "Necker", "Nihoa",
              "Pearl & Hermes", "Molokini", "Lehua", "Kaula")
-df_filtered <- df %>%
-  filter(SCIENTIFIC_NAME %in% species & ISLAND %in% islands) %>%
+nSPC <- c("nSPC")
+
+#filter out for only LUKA/LUFU/CEAR, islands, and spc in method
+df <- df %>%
+  filter(SCIENTIFIC_NAME %in% species & ISLAND %in% islands & METHOD %in% nSPC) %>%
   select("ISLAND", "LATITUDE", "LONGITUDE", "DATE_", "METHOD", "SPECIES", "COUNT", "REGION",
          "DEPTH", "DENSITY")
 
+#rename columns
+df <- df %>%
+  rename(
+    island = ISLAND, lat = LATITUDE, lon = LONGITUDE, date = DATE_, method = METHOD, species = SPECIES, 
+    count = COUNT, region = REGION, depth = DEPTH, density = DENSITY
+  )
 
-sum(df_filtered$SPECIES == "LUKA", na.rm = TRUE) #4216
-sum(df_filtered$SPECIES == "LUFU", na.rm = TRUE) #1956
-sum(df_filtered$SPECIES == "CEAR", na.rm = TRUE) #12056
-colnames(df_filtered)
+#transform lon
+df$lon = ifelse(df$lon < 0, df$lon + 360, df$lon)
 
-df_filtered <- df[, c("ISLAND", "LATITUDE", "LONGITUDE", "DATE_", "METHOD", "SPECIES", "COUNT", "REGION",
-                      "DEPTH", "DENSITY")]
-df_filtered
+#add year, month,day columns
+df <- df %>%
+  mutate(
+    year = year(date),
+    month = month(date),
+    day = day(date)
+  )
+
+#add presence column ASSIGN PRESENCE VALUES 0??
+df <- df %>%
+  mutate(presence = ifelse(density > 0, 1, 0))

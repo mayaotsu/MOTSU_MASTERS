97554e1c27d958c9197a704e58b2978045349eb1
mayaotsu <otsumaya@hawaii.edu>
2025-04-13 23:20:50 -1000

    new dredge

--- script/dredge.R
+++ script/dredge.R
@@ -12,7 +12,7 @@ taape <- taape[taape$species=="LUKA",]
 #na.exclude(taape)
 #na.omit(taape)
 # not including com_net because all NA for NWHI
-taape_full_Gam_2 <-gam(presence~s(depth)+year+s(rugosity)+island+s(mean_1mo_chla_ESA)
+taape_full_Gam <-gam(presence~s(depth)+year+s(rugosity)+island+s(mean_1mo_chla_ESA)
                    +s(q05_1yr_sst_CRW)
                    +s(otp_nearshore_sediment)+s(otp_all_effluent)+s(MHI_spear)+
                    +s(coral_cover),
@@ -20,7 +20,7 @@ taape_full_Gam_2 <-gam(presence~s(depth)+year+s(rugosity)+island+s(mean_1mo_chla
 
 #dredge function
 options(na.action = "na.fail")
-dredge_model <- dredge(taape_full_Gam_2, trace = 5)
+dredge_model <- dredge(taape_full_Gam, trace = 5)
 dredge_model
 saveRDS(dredge_model, "/Users/mayaotsu/Documents/Github/MOTSU_MASTERS/output/gams/taape_full_dredge_2.rds")
 
@@ -89,9 +89,18 @@ toau_mhi_Gam <- gam(presence~s(depth)+year+s(rugosity)+island+s(mean_1mo_chla_ES
                       +s(coral_cover),
                     data = toau_mhi, family = binomial)
 
+options(na.action = "na.fail")
 dredge_toau_mhi <- dredge(toau_mhi_Gam, trace = 5)
 dredge_toau_mhi
-saveRDS(dredge_toau_mhi, "/Users/mayaotsu/Documents/Github/MOTSU_MASTERS/output/gams/toau_mhi_dredge.rds")
+saveRDS(dredge_toau_mhi, "/Users/mayaotsu/Documents/Github/MOTSU_MASTERS/output/gams/toau_mhi_dredge_2.rds")
 
+model.sel(dredge_toau_mhi)[1:10]
+sw(dredge_toau_mhi)
+best_model_4 <- get.models(dredge_toau_mhi, 1)[[1]]
+draw(best_model_4, parametric = TRUE)
+png("/Users/mayaotsu/Documents/Github/MOTSU_MASTERS/output/gams/draw/toau_mhi_draw.png", res = 300, height = 10, width = 10, units = "in")
+par(mfrow=c(3,3))
+draw(best_model_4)
+dev.off()
 model.sel(dredge_toau_mhi)[1:10]
 sw(dredge_toau_mhi)
